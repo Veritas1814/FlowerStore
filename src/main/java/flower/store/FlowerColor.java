52c72902af9174b87696f704bd6e9380a7d3025a
Veritas1814 <149301025+Veritas1814@users.noreply.github.com>
2024-10-12 12:50:53 +0300

    Update FlowerColor.java

--- src/main/java/flower/store/FlowerColor.java
+++ src/main/java/flower/store/FlowerColor.java
@@ -2,9 +2,9 @@ package ucu.apps;
 import lombok.Getter;
 @Getter
 public enum FlowerColor {
-    RED("#FF0000"),WHITE("#FFFFFF"),YELLOW("#FFFF00");
+    RED("#FF0000"), WHITE("#FFFFFF"), YELLOW("#FFFF00");
     private String code;
-    FlowerColor(String code){
-        this.code = code;
+    FlowerColor(final String outcode) {
+        this.code = outcode;
     }
 }
