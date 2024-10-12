33837e179b29f65d7e98665509d80c2a424378a7
Veritas1814 <149301025+Veritas1814@users.noreply.github.com>
2024-10-12 12:48:31 +0300

    Update FlowerPack.java

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -14,12 +14,12 @@ public class FlowerPack {
     private Flower flower;
     private int count;
 
-    public FlowerPack(Flower flower,int count){
+    public FlowerPack(final Flower flower, final int newcount) {
         this.flower = new Flower(flower);
-        this.count = count;
+        this.count = newcount;
     }
-
-    public double getPrice(){
-        return flower.getPrice()*count;
+    
+    public double getPrice() {
+        return flower.getPrice() * count;
     }
 }
